7fb729b33bf95c6f0156cb0303c7a89f4d4f4fed
Antoine GICQUEL <antoine.gicquel@telecom-paris.fr>
2020-02-14 15:37:17 +0100

    Debut de la creation du tool stegano

--- SteganoPython/autocheckstegano.py
+++ SteganoPython/autocheckstegano.py
@@ -1,4 +1,32 @@
-# TODO file, binwalk, strings
+import python-magic
+import binwalk
+import string
+
+## useful functions
+
+def strings(filename, min=4):
+    with open(filename, errors="ignore") as f:
+        result = ""
+        for c in f.read():
+            if c in string.printable:
+                result += c
+                continue
+            if len(result) >= min:
+                yield result
+            result = ""
+        if len(result) >= min:  # catch result at EOF
+            yield result
+
+## main part
+
+# TODO parse args
+
+file_type = magic.from_file(file, mime=True) # equivalent to the 'file' command on Linux
+embedded = binwalk.scan(file) # binwalk scan
+#TODO extract files ?
+file_strings = list(strings(file))
+
+# TODO print the results of basic scans
 
 # TODO si image :
     # TODO lancer exiftool
